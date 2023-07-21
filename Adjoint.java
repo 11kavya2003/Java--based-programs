66f024832f23d903805bc1394ce7a4ff811d120a
kaviyakarikalan <124652847+11kavya2003@users.noreply.github.com>
2023-07-21 08:25:01 +0000

    Add files via upload

---
+++ Adjoint.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Adjoint
+{
+public static void main(String args[ ])
+{int i,j;
+int a[][]={{1,2},{3,4}};
+adjoint[][]=new int[2][2];
+
+for(i=0;i<a.length;i++)
+{
+for( j=0;j<a[0].length:j++)
+{
+a[0][1]=
+}}for(i=0; )
