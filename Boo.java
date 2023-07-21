03194b384f6f2175866d8739028b84d02137fcc6
kaviyakarikalan <124652847+11kavya2003@users.noreply.github.com>
2023-07-21 08:28:06 +0000

    Add files via upload

---
+++ Boo.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Boo
+{
+public static void main(String args[])
+{
+boolean b=false;
+String k = new Boolean(b).toString();
+{
+System.out.println(k);
+}
+
+}
+}
