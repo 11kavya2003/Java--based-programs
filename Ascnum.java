20f9ae099d90be06710fbfe106e85ad3a72b5f2c
kaviyakarikalan <124652847+11kavya2003@users.noreply.github.com>
2023-07-21 08:27:15 +0000

    Add files via upload

---
+++ Ascnum.java
@@ -0,0 +1,19 @@
+import java .util.*;
+
+class Ascnum
+{
+public static void main(String args[])
+{
+
+Scanner sc= new Scanner(System.in);
+System.out.print("Enter the number: ");
+int n= sc.nextInt();
+int i ;
+for(i=1;i<=n;i++)
+{
+ i=i+1;
+}
+System.out.println("The asc numbers are " +i);
+}
+
+}
