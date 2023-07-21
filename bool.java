94af2a50ee5844807821e280620f186247e2d56e
kaviyakarikalan <124652847+11kavya2003@users.noreply.github.com>
2023-07-21 08:28:27 +0000

    Add files via upload

---
+++ bool.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Arrays;
+
+class bool
+{
+public static void main(String args[])
+{
+boolean a1[][]=  {{true,false},{true,false}};
+boolean a2[][]= {{true,false},{true,false}};
+
+
+System.out.println(Arrays.deepEquals(a1,a2));
+}}
