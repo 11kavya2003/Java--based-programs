be5e4155bbd40eb4db695ea592a9d18fd73d7a9a
kaviyakarikalan <124652847+11kavya2003@users.noreply.github.com>
2023-07-21 08:26:05 +0000

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,19 @@
+ //*to find the length of array*//
+
+import java.util.*;
+class Array
+{
+ public static void main(String args[ ])
+{
+int i,n;
+Scanner s= new Scanner(System.in);
+System.out.println("enter the  no of elements of an array");
+ n=s.nextInt();
+int a[ ]=new int[n];
+System.out.println("enter the elements of an array");
+for(i=0;i<=n;i++)
+a[i]=s.nextInt[ ];
+System.out.println(a[5]);
+
+}
+}
